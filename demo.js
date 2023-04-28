dec42533150b131e34de4cd736c70f926bea9e2e
vance <vancedowdell2110@yahoo.com>
2023-04-27 19:36:54 -0700

    new

--- demo.js
+++ demo.js
@@ -2,3 +2,22 @@ const arr=[1,2,3,4,5,6,7,8,9,10]
 for(let index=1;index<=arr.length;index++) {
 console.log(arr[index])
 }
+
+
+
+
+let num = 1
+while(num<=5) {
+    console.log("--------")
+    for(let i=0;i<num;i++)
+    console.log("Iam working on day"+num)
+}
+num++
+
+let string = "vance"
+
+console.log(string[3])
+
+for(let i=0;i<string.length-2;i++) {
+    
+}
