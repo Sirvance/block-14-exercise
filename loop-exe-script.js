9527e23c36cd0dde04c25d3ed47a1552d977cd11
Sirvance <126606183+Sirvance@users.noreply.github.com>
2023-05-27 21:29:18 -0700

    Update loop-exe-script.js

--- loop-exe-script.js
+++ loop-exe-script.js
@@ -1,10 +1,10 @@
 // Write a for loop that prints the numbers 1 through 10 to the console.
-for(let i = 0; i <= 10; i++){
+for (let i = 0; i <= 10; i++) {
     console.log(i)
-}    
+}
 // Write a for loop that prints the even numbers from 2 to 20 to the console.
-for(let i = 0; i <=20; i++){
-    if(i%2===0)console.log(i)
+for (let i = 0; i <= 20; i++) {
+    if(i%2===0) console.log(i)
 }
 // Write a while loop that counts down from 10 to 1 and prints the numbers to the console.
 let i = 10
@@ -23,7 +23,7 @@ while(i>=1){
 // ****
 // *****
 
-for (let i =1; i <= 5; i++) {
+for (let i = 1; i <= 5; i++) {
     let row = "";
     for (let j = 1; j <= i; j++) {
         row += "*";
